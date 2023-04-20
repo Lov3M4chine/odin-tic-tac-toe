6eef4f4f96eb7b8f2fa6c15e6d114e9737302775
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-20 15:38:58 -0600

    Add character select screen layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+    	fontFamily: {
+    		jedi: ['star-jedi', 'sans-serif'],
+    		jedihollow: ['star-jedi-hollow', 'sans-serif'],
+    		starjout: ['star-j-out', 'sans-serif'],
+    		stjedise: ['st-jedi-se', 'sans-serif'],
+    	},
+    },
   },
   plugins: [],
 }
